9d0dca916d6b3b9076527ff9c4b448dfbc8922d1
Shiny Miner <falanshhitkedia123@gmail.com>
2025-02-21 18:49:17 +0530

    Added basic script with full fun!

--- assembly/overworld_scripts/follower_mon.s
+++ assembly/overworld_scripts/follower_mon.s
@@ -10,6 +10,7 @@ EventScript_FollowerMon:
     followerfaceplayer
     lock
     storemonid
+    bufferpokemon 0x0 0x4004
     showpokepic 0x4004 0x15 0x6
     cry 0x4004 0x0
     msgbox gText_FollowerMon_Test MSG_NORMAL
